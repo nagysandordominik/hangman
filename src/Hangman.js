b28f33b3794f09b5ca7fe0910a54db061d9ac015
nagysandordominik <thesatyka@gmail.com>
2023-02-07 19:14:14 +0100

    add: wrong guesses counter

--- src/Hangman.js
+++ src/Hangman.js
@@ -62,8 +62,12 @@ class Hangman extends Component {
       <div className='Hangman'>
         <h1>Hangman</h1>
         <img src={this.props.images[this.state.nWrong]} />
+        <p>Number of wrong guesses:{this.state.nWrong}</p>
+
         <p className='Hangman-word'>{this.guessedWord()}</p>
         <p className='Hangman-btns'>{this.generateButtons()}</p>
+        
+
       </div>
     );
   }
